5804e79a916fa73ad3af47888bed8cf2951c181d
Ian Vergel <cianvergelll@gmail.com>
2025-03-03 16:44:48 +0800

    payment layout

--- app/payments/page.js
+++ app/payments/page.js
@@ -5,7 +5,17 @@ export default function Payments() {
             {/* Left Panel */}
             <div className="h-full w-1/5 border border-red-400 mr-6"></div>
 
-            <div className="h-full w-4/5 border border-red-500"></div>
+            {/* Right Container */}
+            <div className="h-full w-4/5 border border-red-500">
+
+                {/* Calendar */}
+                <div className="w-full h-1/4 border border-red-500 my-6"></div>
+
+                {/* Payment Details & Payment History */}
+                <div className="w-full h-2/3 border border-green-500 mt-3"></div>
+            </div>
+
+
             <div></div>
         </div>
     );
