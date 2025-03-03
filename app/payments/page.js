61392bbb3b5860c5a58630ad279b0c7d6a0d0858
Ian Vergel <cianvergelll@gmail.com>
2025-03-03 16:37:09 +0800

    payments line layout

---
+++ app/payments/page.js
@@ -0,0 +1,12 @@
+export default function Payments() {
+    return (
+        <div className="h-screen w-screen border border-red-500 flex flex-row">
+
+            {/* Left Panel */}
+            <div className="h-full w-1/5 border border-red-400 mr-6"></div>
+
+            <div className="h-full w-4/5 border border-red-500"></div>
+            <div></div>
+        </div>
+    );
+}
